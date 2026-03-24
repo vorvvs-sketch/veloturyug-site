074cb4f4ba1934732778f710ce41cd8045ff86c9
vorvvs-sketch <vor.vvs@gmail.com>
2026-03-24 19:12:11 +0300

    Expand home page content

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,8 @@ import "./globals.css";
 export const metadata = {
   title: "ВелоТур Юг",
   description:
-    "Однодневные туры на электровелосипедах по Краснодару, Краснодарскому краю и Адыгее."
+    "Однодневные туры на электровелосипедах по Краснодару, Краснодарскому краю и Адыгее.",
+  metadataBase: new URL("https://veloturyug.ru")
 };
 
 export default function RootLayout({ children }) {
